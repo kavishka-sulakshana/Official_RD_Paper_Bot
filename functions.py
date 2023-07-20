6e2ab62f1f35187f47e71e7ea9c68401daf07dbc
kavishka-sulakshana <kss20001218@gmail.com>
2023-07-20 23:17:35 +0530

    Finish second part of bot

--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 import requests
 from telegram.ext import (
@@ -13,16 +12,19 @@ import models.storage_functions as storage_functions
 import utils
 import pprint
 
-markup_1 = ReplyKeyboardMarkup(keyBoards.reply_keyboard_1, one_time_keyboard=True)
-markup_2 = ReplyKeyboardMarkup(keyBoards.reply_keyboard_2, one_time_keyboard=True)
-classes_markup = ReplyKeyboardMarkup(keyBoards.reply_keyboard_classes, one_time_keyboard=True)
-markup_close = ReplyKeyboardMarkup(keyBoards.reply_keyboard_close, one_time_keyboard=True)
+markup_1 = ReplyKeyboardMarkup(
+    keyBoards.reply_keyboard_1, one_time_keyboard=True)
+markup_2 = ReplyKeyboardMarkup(
+    keyBoards.reply_keyboard_2, one_time_keyboard=True)
+classes_markup = ReplyKeyboardMarkup(
+    keyBoards.reply_keyboard_classes, one_time_keyboard=True)
+markup_close = ReplyKeyboardMarkup(
+    keyBoards.reply_keyboard_close, one_time_keyboard=True)
+
 
 # Functions for the bot
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    await update.message.reply_text(
-        "🪪 ඔබගේ Barcode අංකය ඇතුලත් කරන්න :"
-    )
+    await update.message.reply_text("🪪 ඔබගේ Barcode අංකය ඇතුලත් කරන්න :")
     return handlers.TYPING_BARCODE
 
 
@@ -34,9 +36,8 @@ async def enter_barcode(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
         "💡 මෙම Bot ඔබගේ ප්‍රශ්න පත්‍ර සහ ප්‍රශ්න පත්‍ර වල ලකුණු ලබාගැනීමට, ප්‍රශ්න පත්‍ර සම්බන්ධ ගැටළු අපවෙත යොමුකිරිම පහසු කිරීම සඳහා ඔබ‍ට සහය වේ.\n\n"
         "✨️ <i>මෙම Bot ගේ සමහර යෙදුම් තවමත් සැකසුම් මට්ටමේ පවතියි.</i>\n\n"
         "🤖 BOT ගෙන් හදිසියේ ඉවත් වීමට අවශ්‍ය නම් /close භාවිත කරන්න..\n\n"
-        "🔥 පහතින් ඔබ‍ට අවශ්‍ය විකල්පයක් තෝරාගන්න..\n\n"
-        , reply_markup=markup_1
-    )
+        "🔥 පහතින් ඔබ‍ට අවශ්‍ය විකල්පයක් තෝරාගන්න..\n\n",
+        reply_markup=markup_1)
     return handlers.CHOOSING
 
 
@@ -55,19 +56,20 @@ async def enter_paper_no(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
             data = classes.get_marks(clz, paper, barcode).to_dict()
             # pprint.pprint(data)
             # studentData = data['student_id'].get().to_dict()
-            await update.message.reply_html(utils.generate_marks_message(
-                        name=barcode,
-                        marks=data['marks'],
-                        Drank=data['rank'],
-                        Arank="-",
-                        link="-",
-                        year=clz,
-                        paper_no=paper,
-                        ptype="ONLINE"
-                    ))
+            await update.message.reply_html(
+                utils.generate_marks_message(name=barcode,
+                                             marks=data['marks'],
+                                             Drank=data['rank'],
+                                             Arank="-",
+                                             link="-",
+                                             year=clz,
+                                             paper_no=paper,
+                                             ptype="ONLINE"))
             await searchingMsg.delete()
         except TypeError:
-            await update.message.reply_text("✉️ Alert -> \n\nඔබ මෙම ප්‍රශ්න පත්‍රය සඳහා පිළිතුරු ලබාදී නොමැත.\n\n ✏️ __")
+            await update.message.reply_text(
+                "✉️ Alert -> \n\nඔබ මෙම ප්‍රශ්න පත්‍රය සඳහා පිළිතුරු ලබාදී නොමැත.\n\n ✏️ __"
+            )
             await searchingMsg.delete()
 
     elif context.user_data["choice"] == "📝  Get Marked Paper":
@@ -75,60 +77,55 @@ async def enter_paper_no(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         chat_id = update.message.chat_id
         filePath = "{}/{}".format(clz, paper)
         fileName = "{}.pdf".format(barcode)
-        searchingMsg = await update.message.reply_sticker('assets/stickers/searching.tgs')
+        searchingMsg = await update.message.reply_sticker(
+            'assets/stickers/searching.tgs')
         # searchingMsg = await update.message.reply_text("🔍 Searching...")
+        print(fileName, filePath)
         try:
             data = classes.get_marks(clz, paper, barcode).to_dict()
             storage_functions.save_file(filePath, fileName)
-            caption = "Marks : {}\n\nRank : {}".format(data['marks'], data['rank'])
-            await bot.send_document(chat_id=chat_id, 
-                            document="temp/{}".format(fileName),
-                            caption=caption)
+            caption = "Marks : {}\n\nRank : {}".format(
+                data['marks'], data['rank'])
+            await bot.send_document(chat_id=chat_id, document="temp/{}".format(fileName), caption=caption)
             storage_functions.delete_local_file("temp/{}".format(fileName))
         except Exception as e:
-            # print(e)
-            await update.message.reply_text("✉️ Alert -> \n\nඔබ මෙම ප්‍රශ්න පත්‍රය සඳහා පිළිතුරු ලබාදී නොමැත.\n\n ✏️ __")
+            print(e)
+            await update.message.reply_text(
+                "✉️ Alert -> \n\nඔබ මෙම ප්‍රශ්න පත්‍රය සඳහා පිළිතුරු ලබාදී නොමැත.\n\n ✏️ __"
+            )
         await searchingMsg.delete()
 
     else:
         await update.message.reply_text("🛡 Invalid Choice !")
 
-    await update.message.reply_text(
-        "🔥 පහතින් ඔබ‍ට අවශ්‍ය විකල්පයක් තෝරාගන්න.. "
-        , reply_markup=markup_1
-    )
+    await update.message.reply_text("🔥 පහතින් ඔබ‍ට අවශ්‍ය විකල්පයක් තෝරාගන්න.. ", reply_markup=markup_1)
     return handlers.CHOOSING
 
 
 async def get_marks(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     text = update.message.text
     context.user_data["choice"] = text
-    await update.message.reply_text(
-        "🪪 ඔබගේ ප්‍රශ්න පත්‍ර අංකය ඇතුලත් කරන්න :"
-    )
+    await update.message.reply_text("🪪 ඔබගේ ප්‍රශ්න පත්‍ර අංකය ඇතුලත් කරන්න :")
     return handlers.TYPING_PAPER
 
 
-async def get_papers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def get_papers(update: Update,
+                     context: ContextTypes.DEFAULT_TYPE) -> int:
     text = update.message.text
     context.user_data["choice"] = text
-    await update.message.reply_text(
-        "🪪 ඔබගේ ප්‍රශ්න පත්‍ර අංකය ඇතුලත් කරන්න :"
-    )
+    await update.message.reply_text("🪪 ඔබගේ ප්‍රශ්න පත්‍ර අංකය ඇතුලත් කරන්න :")
     return handlers.TYPING_PAPER
 
 
-async def paper_issue(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def paper_issue(update: Update,
+                      context: ContextTypes.DEFAULT_TYPE) -> int:
     text = update.message.text
     context.user_data["choice"] = text
-    await update.message.reply_text(
-        "📥 \n\n ඔබගේ ගැටලුව පැහැදිලිව යොමු කරන්න..",
-        reply_markup=markup_close
-    )
+    await update.message.reply_text("📥 \n\n ඔබගේ ගැටලුව පැහැදිලිව යොමු කරන්න..", reply_markup=markup_close)
     return handlers.TYPING_ISSUE
 
 
-async def enter_paper_issue(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def enter_paper_issue(update: Update,   context: ContextTypes.DEFAULT_TYPE) -> int:
     text = update.message.text
     context.user_data["issue"] = text
     data = {
@@ -142,15 +139,11 @@ async def enter_paper_issue(update: Update, context: ContextTypes.DEFAULT_TYPE) 
     if response_data["status"] == "success":
         await update.message.reply_text(
             "\n 👨🏼‍💻 Your Complaint has been recorded !  📥 \n"
-            "\n 🎲  We will check it soon 🔰🔰 \n"
-        )
+            "\n 🎲  We will check it soon 🔰🔰 \n")
     else:
         await update.message.reply_text(response_data["message"])
 
-    await update.message.reply_text(
-        "🔥 පහතින් ඔබ‍ට අවශ්‍ය විකල්පයක් තෝරාගන්න.. "
-        , reply_markup=markup_1
-    )
+    await update.message.reply_text("🔥 පහතින් ඔබ‍ට අවශ්‍ය විකල්පයක් තෝරාගන්න.. ", reply_markup=markup_1)
     return handlers.CHOOSING
 
 
@@ -165,62 +158,48 @@ async def done(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     return ConversationHandler.END
 
 
-async def choose_class(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def choose_class(update: Update,
+                       context: ContextTypes.DEFAULT_TYPE) -> int:
     text = update.message.text
     context.user_data["choice"] = text
-    await update.message.reply_text(
-        "🔥 පහතින් ඔබගේ පන්තිය තෝරාගන්න.. "
-        , reply_markup=classes_markup
-    )
+    await update.message.reply_text("🔥 පහතින් ඔබගේ පන්තිය තෝරාගන්න.. ", reply_markup=classes_markup)
     return handlers.CHOOSING_CLASS
 
 
-async def choose_operation(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def choose_operation(update: Update,  context: ContextTypes.DEFAULT_TYPE) -> int:
     text = update.message.text
     context.user_data["class"] = text
-    await update.message.reply_text(
-        "🔥 පහතින් ඔබ‍ට අවශ්‍ය විකල්පයක් තෝරාගන්න.. "
-        , reply_markup=markup_2
-    )
+    await update.message.reply_text("🔥 පහතින් ඔබ‍ට අවශ්‍ය විකල්පයක් තෝරාගන්න.. ", reply_markup=markup_2)
     return handlers.CHOOSING_OPERATION
 
 
-async def invalid_choice_1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def invalid_choice_1(update: Update,  context: ContextTypes.DEFAULT_TYPE) -> int:
     pprint.pprint(context)
-    await update.message.reply_text(
-        "🛡 Invalid Choice !"
-        , reply_markup=markup_1
-    )
+    await update.message.reply_text("🛡 Invalid Choice !", reply_markup=markup_1)
     return handlers.CHOOSING
 
 
-async def invalid_choice_2(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        await update.message.reply_text(
-            "🛡 Invalid Choice !"
-            , reply_markup=markup_2
-        )
-        return handlers.CHOOSING_OPERATION
+async def invalid_choice_2(update: Update,  context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text("🛡 Invalid Choice !", reply_markup=markup_2)
+    return handlers.CHOOSING_OPERATION
 
 
-async def cancel_issue(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    await update.message.reply_text(
-        "Complaint Closed \n"	
-        "Choose an option : "
-        , reply_markup=markup_1
-    )
+async def cancel_issue(update: Update,
+                       context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text("Complaint Closed \n"
+                                    "Choose an option : ", reply_markup=markup_1)
     return handlers.CHOOSING
 
 
 async def invalid_input_1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text(
-         "✉️ Alert ->\n\nඔබ ලබාදී ඇත්තේ වැරදි barcode අංකයකි.කරුණාකර නිවැරදි Barcode අංකය ලබාදෙන්න.\n\n✏️ __"
+        "✉️ Alert ->\n\nඔබ ලබාදී ඇත්තේ වැරදි barcode අංකයකි.කරුණාකර නිවැරදි Barcode අංකය ලබාදෙන්න.\n\n✏️ __"
     )
     return handlers.TYPING_BARCODE
 
 
 async def invalid_input_2(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text(
-         "✉️ Alert ->\n\nඔබ ලබාදී ඇත්තේ වැරදි ප්‍රශ්න පත්‍ර අංකයකි.කරුණාකර නිවැරදි ප්‍රශ්න පත්‍රයක් ලබාදෙන්න.\n\n✏️ __"
+        "✉️ Alert ->\n\nඔබ ලබාදී ඇත්තේ වැරදි ප්‍රශ්න පත්‍ර අංකයකි.කරුණාකර නිවැරදි ප්‍රශ්න පත්‍රයක් ලබාදෙන්න.\n\n✏️ __"
     )
-    return handlers.TYPING_PAPER    
-    
+    return handlers.TYPING_PAPER
--- handlers.py
+++ handlers.py
@@ -7,73 +7,75 @@ from telegram.ext import (
 )
 
 # States for the bot
-TYPING_BARCODE, CHOOSING, TYPING_PAPER, TYPING_ISSUE, CHOOSING_CLASS, CHOOSING_OPERATION = range(6)
+TYPING_BARCODE, CHOOSING, TYPING_PAPER, TYPING_ISSUE, CHOOSING_CLASS, CHOOSING_OPERATION = range(
+    6)
 
 
 conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", functions.start)],
-        states={
-            CHOOSING: [
-                MessageHandler(
-                    filters.Regex("^🧾  Get Papers$"), functions.choose_class
-                ),
-                MessageHandler(
-                    filters.Regex("^📍 Paper Issues$"), functions.paper_issue
-                ),
-                MessageHandler(
-                    filters.Regex("^❌  Close"), functions.done
-                ),
-                MessageHandler(
-                    filters.TEXT, functions.invalid_choice_1
-                ),
-            ],
-            TYPING_BARCODE: [
-                MessageHandler(
-                    filters.Regex("^[0-9]{8,10}$"), functions.enter_barcode
-                ),
-                CommandHandler("close", functions.done),
-                MessageHandler(
-                    filters.TEXT, functions.invalid_input_1
-                )
-            ],
-            TYPING_PAPER: [
-                MessageHandler(
-                    filters.Regex("^[0-9]{2}$"), functions.enter_paper_no
-                ),
-                CommandHandler("close", functions.done),
-                MessageHandler(
-                    filters.TEXT, functions.invalid_input_2
-                )
-            ],
-            TYPING_ISSUE: [
-                MessageHandler(
-                    filters.Regex("^❌  Cancel$"), functions.cancel_issue
-                ),
-                MessageHandler(
-                    filters.TEXT, functions.enter_paper_issue
-                )
-            ],
-            CHOOSING_CLASS: [
-                CommandHandler("close", functions.done),
-                MessageHandler(
-                    filters.TEXT, functions.choose_operation
-                )
-            ],
-            CHOOSING_OPERATION: [
-                MessageHandler(
-                    filters.Regex("^🔖  Get Marks$"), functions.get_marks
-                ),
-                MessageHandler(
-                    filters.Regex("^📝  Get Marked Paper$"), functions.get_papers
-                ),
-                MessageHandler(
-                    filters.Regex("^❌  Close"), functions.done
-                ),
-                MessageHandler(
-                    filters.TEXT, functions.invalid_choice_2
-                ),
-            ]
-        },
-        fallbacks=[
-            MessageHandler(filters.Regex("^❌  Close"), functions.done)],
-    )
+    entry_points=[CommandHandler("start", functions.start)],
+    states={
+        CHOOSING: [
+            MessageHandler(
+                filters.Regex("^🧾  Get Papers$"), functions.choose_class
+            ),
+            MessageHandler(
+                filters.Regex("^📍 Paper Issues$"), functions.paper_issue
+            ),
+            MessageHandler(
+                filters.Regex("^❌  Close"), functions.done
+            ),
+            MessageHandler(
+                filters.TEXT, functions.invalid_choice_1
+            ),
+        ],
+        TYPING_BARCODE: [
+            MessageHandler(
+                filters.Regex("^[0-9]{8,10}$"), functions.enter_barcode
+            ),
+            CommandHandler("close", functions.done),
+            MessageHandler(
+                filters.TEXT, functions.invalid_input_1
+            )
+        ],
+        TYPING_PAPER: [
+            MessageHandler(
+                filters.Regex("^[0-9]*$"), functions.enter_paper_no
+            ),
+            CommandHandler("close", functions.done),
+            MessageHandler(
+                filters.TEXT, functions.invalid_input_2
+            )
+        ],
+        TYPING_ISSUE: [
+            MessageHandler(
+                filters.Regex("^❌  Cancel$"), functions.cancel_issue
+            ),
+            MessageHandler(
+                filters.TEXT, functions.enter_paper_issue
+            )
+        ],
+        CHOOSING_CLASS: [
+            CommandHandler("close", functions.done),
+            MessageHandler(
+                filters.TEXT, functions.choose_operation
+            )
+        ],
+        CHOOSING_OPERATION: [
+            MessageHandler(
+                filters.Regex("^🔖  Get Marks$"), functions.get_marks
+            ),
+            MessageHandler(
+                filters.Regex(
+                    "^📝  Get Marked Paper$"), functions.get_papers
+            ),
+            MessageHandler(
+                filters.Regex("^❌  Close"), functions.done
+            ),
+            MessageHandler(
+                filters.TEXT, functions.invalid_choice_2
+            ),
+        ]
+    },
+    fallbacks=[
+        MessageHandler(filters.Regex("^❌  Close"), functions.done)],
+)
